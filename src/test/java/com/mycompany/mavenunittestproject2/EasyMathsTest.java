ef1d11dd7fe574eb3014af96b0044c7a54368e7d
Brendan.Watson <Brendan.Watson@LECTERN_8A106.litdom.lit.ie>
2016-04-21 12:19:44 +0100

    initial committ

---
+++ src/test/java/com/mycompany/mavenunittestproject2/EasyMathsTest.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.mavenunittestproject2;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Brendan.Watson
+ */
+public class EasyMathsTest {
+    
+    public EasyMathsTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    
+    
+}
